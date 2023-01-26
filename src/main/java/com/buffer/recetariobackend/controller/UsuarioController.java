ee959608f7a8b967a2bd36e760d141d3f353414a
Germán Altairac <germanaltairac@hotmail.com>
2023-01-26 05:55:32 -0300

    CORS

--- src/main/java/com/buffer/recetariobackend/controller/UsuarioController.java
+++ src/main/java/com/buffer/recetariobackend/controller/UsuarioController.java
@@ -5,11 +5,9 @@ import com.buffer.recetariobackend.entity.Usuario;
 import com.buffer.recetariobackend.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users")
 public class UsuarioController {
