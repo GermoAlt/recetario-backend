b8d2ed0b882366750b8625792033fd84e3476ff3
Tuckson <franrache@gmail.com>
2023-03-10 17:14:55 -0300

    initial commit

---
+++ src/main/java/com/buffer/recetariobackend/controller/CalificacionController.java
@@ -0,0 +1,36 @@
+package com.buffer.recetariobackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buffer.recetariobackend.entity.Calificacion;
+import com.buffer.recetariobackend.service.ICalificacionService;
+
+@CrossOrigin()
+
+@RestController
+@RequestMapping("/api/calificaciones")
+public class CalificacionController {
+
+  @Autowired
+    private ICalificacionService calificacionService;
+ 
+  
+        
+      }
+      @PostMapping("/{id}")
+      public ResponseEntity<Calificacion> calificar(@PathVariable String id, @RequestBody Calificacion Calificacion ){
+        return null;
+      }
+    
+
+}
---
+++ src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
@@ -0,0 +1,12 @@
+package com.buffer.recetariobackend.service;
+
+import java.util.Optional;
+
+import com.buffer.recetariobackend.entity.Calificacion;
+
+public interface ICalificacionService {
+
+    
+
+
+}
